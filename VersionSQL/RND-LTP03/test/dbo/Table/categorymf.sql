46f354aef443147a648d4050d10bdad1629c81d5
desperadomar <desperadomar@gmail.com>
2016-11-23 00:20:57 +0400

    added name column

--- VersionSQL/RND-LTP03/test/dbo/Table/categorymf.sql
+++ VersionSQL/RND-LTP03/test/dbo/Table/categorymf.sql
@@ -5,6 +5,7 @@ SET QUOTED_IDENTIFIER ON
 CREATE TABLE [dbo].[categorymf](
 	[Category Type] [nvarchar](255) NULL,
 	[Category] [nvarchar](255) NULL,
-	[MF] [float] NULL
+	[MF] [float] NULL,
+	[name] [varchar](1) NULL
 ) ON [PRIMARY]
 
