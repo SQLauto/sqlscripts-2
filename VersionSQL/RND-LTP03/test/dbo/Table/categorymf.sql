a09d5d000c682222060b3b6ecf25c7a1daafc796
desperadomar <desperadomar@gmail.com>
2016-11-23 00:19:19 +0400

    new test

---
+++ VersionSQL/RND-LTP03/test/dbo/Table/categorymf.sql
@@ -0,0 +1,10 @@
+/****** Object:  Table [dbo].[categorymf]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[categorymf](
+	[Category Type] [nvarchar](255) NULL,
+	[Category] [nvarchar](255) NULL,
+	[MF] [float] NULL
+) ON [PRIMARY]
+
